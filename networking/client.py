c68b0fb463e50d6fdbb9808260a628014b28a0eb
Haris Smajlovic <hsmajlovic@buna.cs.uvic.ca>
2020-11-19 11:58:29 -0800

    Basic arithmetics volume implemented

--- networking/client.py
+++ networking/client.py
@@ -1,14 +1,45 @@
 from typing import Callable, Any
+from collections import defaultdict
 
+from utils.constants import CP0, CP1, CP2
 
 class Client:
+    # TODO: Make this a connection class only. Separate server specific stuff into a different class.
     def __init__(self: 'Client', pid: int):
-        # TODO: Implement Client class as server
-
         self.pid = pid
-
+        self.__context = defaultdict(list)
+        self.__other_clients = dict()
+    
+    def client_connect(self: 'Client', other: 'Client'):
+        # Temp local solution
+        self.__other_clients[other.pid] = other
+    
+    def client_disconnect(self: 'Client', other: 'Client'):
+        # Temp local solution:
+        del self.__other_clients[other.pid]
 
     def call(self: 'Client', secure_fn: Callable, *args) -> Any:
         # TODO: Implement async/sync callstack
         
         return secure_fn(*args)
+    
+    def append_to_context(self: 'Client', context_id: int, shared: Any):
+        self.__context[context_id].append(shared)
+    
+    def prune_context(self: 'Client', context_id: int):
+        del self.__context[context_id]
+    
+    def get_shared(self: 'Client', context_id: int, secrets: list, transform: Callable) -> Any:
+        return transform([self.__context[context_id][i] for i in secrets])
+
+    def get_counter_client(self: 'Client') -> 'Client':
+        if self.pid == CP0:
+            raise ValueError('Preprocess client has no counter client.')
+        
+        return self.__other_clients[int(not self.pid)]
+
+    def reconstruct_secret(self: 'Client', context_id: int, secrets: list, transform: Callable) -> Any:
+        self_shared = self.get_shared(context_id, secrets, transform)
+        other_shared = self.get_counter_client().get_shared(context_id, secrets, transform)
+
+        return self_shared + other_shared
--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,5 @@
 NUMBER_OF_CLIENTS: int = 3
+CP0: int = -1
+CP1: int = 0
+CP2: int = 1
 
