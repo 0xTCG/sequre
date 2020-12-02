f56018b3190b4f8bd856d7504a1bcbcf70d91505
Haris Smajlovic <hsmajlovic@buna.cs.uvic.ca>
2020-12-02 15:29:59 -0800

    Inputs can be all public now

--- sequre.py
+++ sequre.py
@@ -17,24 +17,24 @@ class Sequre:
     def __exit__(self: 'Sequre', exc_type, exc_val, exc_tb):
         self.mf.__exit__(exc_type, exc_val, exc_tb)
 
-    def add(self: 'Sequre', x: Any, y: Any) -> Any:
+    def add(self: 'Sequre', x: Any, y: Any, share_inputs: bool) -> Any:
         return self.mf(arithmetics.add)(
-            x, y, secret_args_mask='11')
+            x, y, secret_args_mask=3 * share_inputs)
 
-    def add_public(self: 'Sequre', x: Any, a: Any) -> Any:
+    def add_public(self: 'Sequre', x: Any, a: Any, share_inputs: bool) -> Any:
         return self.mf(arithmetics.add_scalar)(
-            x, a, secret_args_mask='10')
+            x, a, secret_args_mask=2 * share_inputs)
     
-    def multiply_public(self: 'Sequre', x: Any, a: Any) -> Any:
+    def multiply_public(self: 'Sequre', x: Any, a: Any, share_inputs: bool) -> Any:
         return self.mf(arithmetics.multiply_scalar)(
-            x, a, secret_args_mask='10')
+            x, a, secret_args_mask=2 * share_inputs)
     
-    def multiply(self: 'Sequre', x: Any, y: Any) -> Any:
+    def multiply(self: 'Sequre', x: Any, y: Any, share_inputs: bool) -> Any:
         return self.mf(arithmetics.multiply)(
-            x, y, secret_args_mask='11',
+            x, y, secret_args_mask=3 * share_inputs,
             preprocess=preprocess.get_multiplication_triple)
 
-    def evaluate_polynomial(self: 'Sequre', x: Vector, coef: list, degrees_list: list) -> Any:
+    def evaluate_polynomial(self: 'Sequre', x: Vector, coef: list, degrees_list: list, share_inputs: bool) -> Any:
         return self.mf(arithmetics.evaluate_polynomial)(
-            x, coef, degrees_list, secret_args_mask='100',
+            x, coef, degrees_list, secret_args_mask=4 * share_inputs,
             preprocess=preprocess.beaver_partition)
