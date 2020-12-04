6911a3edeed7c7b2495d8429df2d39b0ec2ce1b6
Haris Smajlovic <hsmajlovic@buna.cs.uvic.ca>
2020-12-03 22:05:43 -0800

    Table lookup init

--- mpc/preprocess.py
+++ mpc/preprocess.py
@@ -16,7 +16,7 @@ def get_multiplication_triple(client: Client, context_id: int) -> List[tuple]:
         share_secret(clients=client.get_other_clients(), value=e, context_id=context_id)
 
 
-def beaver_partition(client: Client, context_id: int) -> List[tuple]:
+def beaver_partition(client: Client, context_id: int) -> Vector:
     computing_clients: list = client.get_other_clients()
     vector_len: int = len(computing_clients[0].get_param(context_id, 0))
     peaky_blinders: Vector = Vector([generate_random_number()] * vector_len)
@@ -32,8 +32,17 @@ def beaver_partition(client: Client, context_id: int) -> List[tuple]:
     
     share_secret(clients=computing_clients, context_id=context_id, value=x_r, private=False)
     
+    return peaky_blinders
+
+
+def polinomial_partition(client: Client, context_id: int, degrees_list: list = None):
+    peaky_blinders: Vector = beaver_partition(client=client, context_id=context_id)
+    
+    computing_clients: list = client.get_other_clients()
     all_term_masks = set()
-    for degrees in computing_clients[0].get_param(context_id, 2):
+    degrees_list = computing_clients[0].get_param(context_id, 2) if degrees_list is None else degrees_list
+
+    for degrees in degrees_list:
         all_term_masks |= set(term_masks(degrees))
     all_term_masks = sorted(list(all_term_masks))
     
@@ -41,4 +50,11 @@ def beaver_partition(client: Client, context_id: int) -> List[tuple]:
         math.prod([peaky_blinder ** exponent for peaky_blinder, exponent in zip(peaky_blinders, term_mask)])
         for term_mask in all_term_masks])
     share_secret(clients=computing_clients, context_id=context_id, value=term_values)
-    
+
+
+def lookup_partition(client: Client, context_id: int):
+    computing_clients: list = client.get_other_clients()
+    table_id: int = computing_clients[0].get_param(context_id, 1)
+    lagrange_coefs: Vector = Vector(computing_clients[0].get_lagrange_coefficients(table_id))
+    degrees: list = [[i] for i in range(len(lagrange_coefs))]
+    polinomial_partition(client, context_id, degrees)
