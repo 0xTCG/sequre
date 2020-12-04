5a75921e023b5204c1072c2504173d7211f4d662
Haris Smajlovic <hsmajlovic@buna.cs.uvic.ca>
2020-12-04 15:32:46 -0800

    Clean sheet

--- utils/constants.py
+++
@@ -1,5 +0,0 @@
-NUMBER_OF_CLIENTS: int = 3
-CP0: int = -1
-CP1: int = 0
-CP2: int = 1
-
--- utils/lambdas.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-
-def subtract(args: list) -> Any:
-    if len(args) != 2:
-        raise ValueError('Invalid params for subtraction!')
-    
-    return args[0] - args[1]
