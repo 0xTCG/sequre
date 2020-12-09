f55ee8b746648e617ffde065194e9d2db4c04783
Haris Smajlovic <hsmajlovic@buna.cs.uvic.ca>
2020-12-08 20:23:40 -0800

    Powers done

--- custom_types.py
+++ custom_types.py
@@ -51,6 +51,10 @@ class Zp:
     
     def to_bytes(self: 'Zp') -> bytes:
         return self.value.to_bytes((self.value.bit_length() + 7) // 8, 'big')
+    
+    @staticmethod
+    def randzp() -> 'Zp':
+        return Zp(random.randint(0, BASE_P))
 
 
 class Vector:
@@ -87,19 +91,28 @@ class Vector:
         return z
     
     def __eq__(self: 'Vector', other: 'Vector') -> bool:
-        return self.value == other.value
+        for e, o in zip(self.value, other.value):
+            if e != o:
+                return False
+        return True
     
     def __neq__(self: 'Vector', other: 'Vector') -> bool:
-        return self.value != other.value
+        return not self == other
     
     def __str__(self: 'Vector') -> str:
-        return str(self.value)
+        return str([str(e) for e in self.value])
     
     def __hash__(self: 'Vector') -> int:
         return hash(tuple(self.value))
     
     def __len__(self: 'Vector') -> int:
         return len(self.value)
+    
+    def __getitem__(self: 'Vector', idx: int) -> object:
+        return self.value[idx]
+    
+    def __setitem__(self: 'Vector', idx: int, value: object):
+        self.value[idx] = value
 
     def to_bytes(self: 'Vector') -> bytes:
         return b'.'.join([bytes(str(e), encoding='utf8') for e in self.value])
@@ -108,3 +121,38 @@ class Vector:
         if self.type_ is None:
             self.type_ = type(elem)
         self.value.append(elem)
+
+
+# This inheritance will be easy to refactor to .seq via extend
+class Matrix(Vector):
+    def __init__(self: 'Matrix', m: int = 0, n: int = 0, randomise: bool = False):
+        self.value = [
+            Vector([(Zp.randzp() if randomise else Zp(0)) for _ in range(n)])
+            for _ in range(m)]
+        self.type_ = Vector
+    
+    def __str__(self: 'Matrix') -> str:
+        s: str = '\n'
+        for v in self.value:
+            s += f'{v}\n'
+        
+        return s
+    
+    def from_value(self: 'Matrix', value: Vector) -> 'Matrix':
+        self.value = value
+        return self
+    
+    def set_dims(self: 'Matrix', m: int, n: int):
+        new_mat: list = [Vector([Zp(0)] * n) for _ in range(m)]
+
+        for i, row in enumerate(self.value):
+            for j, cell in enumerate(row):
+                new_mat[i][j] = cell
+        
+        self.value = new_mat
+    
+    def num_rows(self: 'Matrix') -> int:
+        return len(self.value)
+    
+    def to_bytes(self: 'Matrix') -> bytes:
+        return b';'.join([v.to_bytes() for v in self.value])
