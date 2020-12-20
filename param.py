cfe7bf8d3861f067250e5f9d354e92628870def2
Haris Smajlovic <hsmajlovic@buna.cs.uvic.ca>
2020-12-19 23:42:31 -0800

    Logireg protocol preliminary done
    Former-commit-id: bca8efbe5d72c3e0eec541db8c7aec2824c21936

--- param.py
+++ param.py
@@ -14,6 +14,9 @@ ALL_PORTS: list = [PORT_P0_P1, PORT_P0_P2, PORT_P1_P2, PORT_P1_P3, PORT_P2_P3]
 
 AF_PREFIX: str = "./_socket"
 
+OUTPUT_FILE_PREFIX: str = "out/test"
+CACHE_FILE_PREFIX: str = "cache/test"
+
 BASE_P: int = 1461501637330902918203684832716283019655932542929
 NBIT_K: int = 60
 NBIT_F: int = 45
@@ -23,3 +26,8 @@ DIV_MAX_N = 100000
 
 BASE_LEN: int = math.ceil(math.log10(BASE_P))
 ITER_PER_EVAL: int = 5
+
+NUM_INDS: int = 1000
+NUM_SNPS = 1000
+NUM_COVS: int = 10
+NUM_DIM_TO_REMOVE = 5
--- utils.py
+++ utils.py
@@ -3,3 +3,12 @@ import param
 
 def get_address(port: int) -> str:
     return f"{param.AF_PREFIX}.{port}"
+
+def get_cache_path(pid: int, name: str) -> str:
+    return f'{param.CACHE_FILE_PREFIX}_P{pid}_{name}.bin'
+
+def get_output_path(pid: int, name: str) -> str:
+    return f'{param.OUTPUT_FILE_PREFIX}_P{pid}_{name}.txt'
+
+def get_temp_path(pid: int, name: str) -> str:
+    return f'temp/temp_P{pid}_{name}.txt'
