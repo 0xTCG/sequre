5b41229aae7e56316a6b0296c4c59289abd50785
harissmajlovic <smajlovicharis@hotmail.com>
2021-01-26 20:59:28 -0800

    Tidying code pt. 2/3
    Former-commit-id: a3bb192a1bbfb7896ac630ff27123772d225b2d1

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import utils.param as param
 
+from utils.custom_types import zeros
+
 
 def get_address(port: int) -> str:
     return f"{param.AF_PREFIX}.{port}"
@@ -22,9 +24,38 @@ def get_temp_path(pid: int, name: str) -> str:
 
 
 def rand_int(lower_limit: int, upper_limit: int) -> int:
-    return np.random.randint(lower_limit, upper_limit)
+    return 1
+    # return np.random.randint(lower_limit, upper_limit)
+
+
+def random_ndarray(base: int, shape: tuple) -> np.ndarray:
+    return zeros(shape) + 1
+    # return np.random.randint(base, size=shape)
 
 
 def bytes_to_arr(bytes_str: str) -> np.ndarray:
     for elem in bytes_str.split(b'.'):
         yield int(elem)
+
+
+def read_vector(f, n: int, field: int) -> np.ndarray:
+    a: np.ndarray = zeros(n)
+    primes_bytes = list()  # TODO: Pass as param.
+    
+    for i in range(n):
+        a[i] = int(f.read(primes_bytes[0]))
+    
+    return a
+
+
+def read_matrix(f, nrows: int, ncols: int, field: int) -> np.ndarray:
+    a: np.ndarray = zeros((nrows, ncols))
+    
+    for i in range(nrows):
+        a[i][:] = read_vector(f, ncols, field)
+    
+    return a
+
+
+def filter_rows(mat: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    return filter(mat, mask)
