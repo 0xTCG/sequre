6d9bb8b458229b9959e55e7753747e4bfea370dd
Haris Smajlović <hsmajlov@gmail.com>
2021-07-12 20:05:46 -0700

    .gitignore updated
    Former-commit-id: fddd606d2cafdf4e38aec4b698ba6222b82cf4b5

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Copying DSL to Seq ..."
-cp -ru dsl/* seq/stdlib/sequre/
-echo "Running tests ..."
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    export CP_OPTIONS=-ru
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    export CP_OPTIONS=-R
+fi
+
+cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
+
+echo "Compiling tests ..."
 seq/build/seqc run -release client.seq test
