db26f04af8d84ce09520c3763fcfc9bdf6329695
Haris Smajlović <smajlovicharis@hotmail.com>
2021-07-12 20:05:46 -0700

    .gitignore updated

--- scripts/run_playground.sh
+++ scripts/run_playground.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Copying DSL to Seq ..."
-cp -ru dsl/* seq/stdlib/sequre/
-echo "Running playground ..."
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    export CP_OPTIONS=-ru
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    export CP_OPTIONS=-R
+fi
+
+cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
+echo "Compiling playground ..."
 GC_INITIAL_HEAP_SIZE=838612000 seq/build/seqc run -release client.seq test --test-run
