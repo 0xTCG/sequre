084d4cef0e51af8db915aa850478b7c023180004
Haris Smajlović <smajlovicharis@hotmail.com>
2021-07-16 23:20:46 -0700

    Bug found
    Former-commit-id: 27ad68f5139088338ff59fff27e684a6ac77bbcd

--- scripts/run_playground.sh
+++ scripts/run_playground.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Cleaning up sockets ..."
+find  . -name 'sock.*' -exec rm {} \;
+
 echo "Copying DSL to Seq ..."
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -11,3 +14,6 @@ fi
 cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
 echo "Compiling playground ..."
 GC_INITIAL_HEAP_SIZE=838612000 seq/build/seqc run -release client.seq test --test-run
+
+echo "Cleaning up sockets ..."
+find  . -name 'sock.*' -exec rm {} \;
