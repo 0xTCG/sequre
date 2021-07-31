8635e5e6783643f5dc8170836e3a491994c53673
Haris Smajlović <smajlovicharis@hotmail.com>
2021-07-31 10:52:33 -0700

    Update CIs

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -15,9 +15,9 @@ cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
 
 echo "Compiling tests ..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    /usr/bin/time -v seq/build/seqc run -release client.seq test
+    /usr/bin/time -v seq/build/seqc run -release client.seq test-all
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    seq/build/seqc run -release client.seq test
+    seq/build/seqc run -release client.seq test-all
 fi
 
 echo "Cleaning up sockets ..."
