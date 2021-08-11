e0af03bc937919fc03ecb5fc4440b7a326b96a9f
Haris Smajlović <smajlovicharis@hotmail.com>
2021-08-10 18:03:34 -0700

    Probing #46 fix
    Former-commit-id: 2b92608559026631586f17b10ea9b2d890d63730

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -15,9 +15,9 @@ cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
 
 echo "Compiling tests ..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    /usr/bin/time -v seq/build/seqc run -release client.seq test-$1
+    /usr/bin/time -v seq/build/seqc run client.seq test-$1
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    seq/build/seqc run -release client.seq test-$1
+    seq/build/seqc run client.seq test-$1
 fi
 
 echo "Cleaning up sockets ..."
