1f490644cdb01cc60e64d373c6ec240c81bf4cd9
Haris Smajlović <smajlovicharis@hotmail.com>
2022-06-28 16:08:58 -0700

    INET sockets configured
    Former-commit-id: 7352c0e17f8daa29df5fcfa531f35d44c8395ad5

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,12 +12,17 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
+if [[ ${*:3} == *"--local"* ]]; then
+    mv seq/stdlib/sequre/network/unix_socket.seq seq/stdlib/sequre/network/socket.seq
+else
+    mv seq/stdlib/sequre/network/inet_socket.seq seq/stdlib/sequre/network/socket.seq
+fi
 
 echo "Compiling $2 ..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8144000000 /usr/bin/time -v seq/build/seqc run -$1 client.seq run-$2 $3
+    GC_INITIAL_HEAP_SIZE=8144000000 /usr/bin/time -v seq/build/seqc run -$1 client.seq run-$2 ${*:3}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8144000000 seq/build/seqc run -$1 client.seq run-$2 $3
+    GC_INITIAL_HEAP_SIZE=8144000000 seq/build/seqc run -$1 client.seq run-$2 ${*:3}
 fi
 
 echo "Cleaning up sockets ..."
