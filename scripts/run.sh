5593353303696d2df8fd7cc88062d613b38482b0
Haris Smajlović <smajlovicharis@hotmail.com>
2022-07-06 12:29:22 -0700

    Fixing GWAS

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,17 +12,17 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
-if [[ ${*:3} == *"--local"* ]]; then
+if [[ ${*:2} == *"--local"* ]]; then
     mv seq/stdlib/sequre/network/unix_socket.seq seq/stdlib/sequre/network/socket.seq
 else
     mv seq/stdlib/sequre/network/inet_socket.seq seq/stdlib/sequre/network/socket.seq
 fi
 
-echo "Compiling $2 ..."
+echo "Compiling $1 ..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8589934592 /usr/bin/time -v seq/build/seqc run -$1 client.seq run-$2 ${*:3}
+    GC_INITIAL_HEAP_SIZE=8589934592 /usr/bin/time -v seq/build/seqc run -release client.seq run-$1 ${*:2}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8589934592 seq/build/seqc run -$1 client.seq run-$2 ${*:3}
+    GC_INITIAL_HEAP_SIZE=8589934592 seq/build/seqc run -release client.seq run-$1 ${*:2}
 fi
 
 echo "Cleaning up sockets ..."
