cb9ebf9fcb410bac67a4be3956c06ed10dbeb834
Haris Smajlović <smajlovicharis@hotmail.com>
2022-07-26 16:05:53 -0700

    Ganon minor updates
    Former-commit-id: be0db57d862236fe9ab8d3c77266e139aa54753a

--- scripts/run.sh
+++ scripts/run.sh
@@ -20,9 +20,9 @@ fi
 
 echo "Compiling $1 ..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8589934592 /usr/bin/time -v seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
+    GC_INITIAL_HEAP_SIZE=17179869184 /usr/bin/time -v seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8589934592 seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
+    GC_INITIAL_HEAP_SIZE=17179869184 seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
 fi
 
 echo "Cleaning up sockets ..."
