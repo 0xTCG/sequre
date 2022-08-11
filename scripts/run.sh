85c49643564c5c972af5db2f6398f708580832d6
Haris Smajlović <smajlovicharis@hotmail.com>
2022-08-10 17:50:15 -0700

    Generics updated
    Former-commit-id: 1574c8e8883f100e27a99523186bfd4125a4a1af

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,18 +11,18 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     export CP_OPTIONS=-Rf
 fi
 
-cp $CP_OPTIONS dsl/* seq/stdlib/sequre/
+cp $CP_OPTIONS dsl/* new_seq/stdlib/sequre/
 if [[ ${*:2} == *"--local"* ]]; then
-    mv seq/stdlib/sequre/network/unix_socket.seq seq/stdlib/sequre/network/socket.seq
+    mv new_seq/stdlib/sequre/network/unix_socket.seq new_seq/stdlib/sequre/network/socket.seq
 else
-    mv seq/stdlib/sequre/network/inet_socket.seq seq/stdlib/sequre/network/socket.seq
+    mv new_seq/stdlib/sequre/network/inet_socket.seq new_seq/stdlib/sequre/network/socket.seq
 fi
 
 echo "Compiling $1 ..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8179869184 GC_LIMIT=8179869184 /usr/bin/time -v seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
+    GC_INITIAL_HEAP_SIZE=8179869184 GC_LIMIT=8179869184 /usr/bin/time -v new_seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    GC_INITIAL_HEAP_SIZE=8179869184 GC_LIMIT=8179869184 seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
+    GC_INITIAL_HEAP_SIZE=8179869184 GC_LIMIT=8179869184 new_seq/build/seqc run -release scripts/invoke.seq run-$1 ${*:2}
 fi
 
 echo "Cleaning up sockets ..."
