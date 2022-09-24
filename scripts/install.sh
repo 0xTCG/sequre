17fe0625a5cfc07d53d61846d703b9762b6301d5
Haris Smajlović <smajlovicharis@hotmail.com>
2022-09-24 10:45:20 -0700

    Install script updated
    Former-commit-id: 8546fa1f6d411c27249cad54fbf0dd45a5e32986

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,3 @@
-export CC=clang
-export CXX=clang++ 
-
 git clone --depth 1 -b release/12.x https://github.com/llvm/llvm-project
 mkdir -p llvm-project/llvm/build
 cd llvm-project/llvm/build
@@ -21,18 +18,10 @@ git checkout feature/sequre-v0.0.1
 mkdir stdlib/sequre
 cp -r dsl/* stdlib/sequre/
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    export LIBEXT=so
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    export LIBEXT=dylib
-fi
 
-mkdir build
-ln -s $(pwd)/deps/lib/libomp.${LIBEXT} $(pwd)/build/libomp.${LIBEXT}
-export SEQ_HTSLIB=$(pwd)/deps/lib/libhts.${LIBEXT}
 mkdir build
 (cd build && cmake .. -DCMAKE_BUILD_TYPE=Release \
                       -DLLVM_DIR=$(llvm-config --cmakedir) \
-                      -DCMAKE_C_COMPILER=${CC} \
-                      -DCMAKE_CXX_COMPILER=${CXX})
+                      -DCMAKE_C_COMPILER=clang \
+                      -DCMAKE_CXX_COMPILER=clang++ )
 cmake --build build --config Release
