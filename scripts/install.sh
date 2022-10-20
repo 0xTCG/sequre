e9fb3d1d59eed5537590a3adcae489bd8b8e6bf5
Haris Smajlović <hsmajlov@gmail.com>
2022-10-20 10:28:44 -0700

    Install script update
    Former-commit-id: 1a182c21e13182cec30e28901f495eab2790dae2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,13 @@
 git clone --depth 1 -b release/12.x https://github.com/llvm/llvm-project
 mkdir -p llvm-project/llvm/build
-cd llvm-project/llvm/build
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=$(pwd)/../../../llvm-build
+(cd llvm-project/llvm/build && cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/llvm-build
     -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_INCLUDE_TESTS=OFF \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_ENABLE_ZLIB=OFF \
     -DLLVM_ENABLE_TERMINFO=OFF \
-    -DLLVM_TARGETS_TO_BUILD=host
-make
-make install
-cd ../../..
+    -DLLVM_TARGETS_TO_BUILD=host && make && make install)
 
 git clone https://github.com/HarisSmajlovic/seq.git
 cd seq
@@ -21,10 +17,8 @@ mkdir stdlib/sequre
 cp -r ../dsl/* stdlib/sequre/
 
 mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DLLVM_DIR=$(pwd)/../../llvm-build/lib/cmake/llvm \
+(cd build && cmake .. -DCMAKE_BUILD_TYPE=Release \
+         -DLLVM_DIR=$HOME/llvm-build/lib/cmake/llvm \
          -DCMAKE_C_COMPILER=clang \
-         -DCMAKE_CXX_COMPILER=clang++
-cd ..
+         -DCMAKE_CXX_COMPILER=clang++)
 cmake --build build --config Release
