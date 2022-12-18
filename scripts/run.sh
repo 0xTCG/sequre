f77fe05031fa72f67f5578b02c7c2e27798b7104
Haris Smajlović <hsmajlov@gmail.com>
2022-12-17 20:51:03 -0800

    Minor Lattiseq benchmark update
    Former-commit-id: 38daedd3b3c0f7974595976ea618879469a392af

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,9 +19,9 @@ else
     cp $CP_OPTIONS $SEQURE_STDLIB/network/inet_socket.codon $SEQURE_STDLIB/network/socket.codon
 fi
 
-echo "Compiling $1 ..."
 echo "Seq plugin searched at $SEQ_PATH ..."
 echo "Sequre plugin searched at $SEQURE_PATH ..."
+echo "Compiling $1 ..."
 GC_INITIAL_HEAP_SIZE=8179869184 GC_LIMIT=8179869184 codon run -plugin $SEQURE_PATH -plugin $SEQ_PATH -release scripts/invoke.codon run-$1 ${*:2}
 
 echo "Cleaning up sockets ..."
