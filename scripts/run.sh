29e8e569e5860464713d57981f4c41bcad71545b
Haris Smajlović <hsmajlov@gmail.com>
2023-01-06 16:59:31 -0800

    Gaussian and uniform read_lvl vectorized
    Former-commit-id: 5d540b6afed72fb766cc0dd17046794699f128ca

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,7 +13,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     export CP_OPTIONS=-f
 fi
 
-if [[ ${*:2} == *"--local"* ]]; then
+if [[ ${*:3} == *"--local"* ]]; then
     cp $CP_OPTIONS $SEQURE_STDLIB/network/unix_socket.codon $SEQURE_STDLIB/network/socket.codon
 else
     cp $CP_OPTIONS $SEQURE_STDLIB/network/inet_socket.codon $SEQURE_STDLIB/network/socket.codon
@@ -21,8 +21,8 @@ fi
 
 echo "Seq plugin searched at $SEQ_PATH ..."
 echo "Sequre plugin searched at $SEQURE_PATH ..."
-echo "Compiling $1 ..."
-GC_INITIAL_HEAP_SIZE=8179869184 GC_LIMIT=8179869184 codon run -plugin $SEQURE_PATH -plugin $SEQ_PATH -release scripts/invoke.codon run-$1 ${*:2}
+echo "Compiling $2 in $1 mode ..."
+codon run -plugin $SEQURE_PATH -plugin $SEQ_PATH $1 scripts/invoke.codon run-$2 ${*:3}
 
 echo "Cleaning up sockets ..."
 find  . -name 'sock.*' -exec rm {} \;
