1be2aa07b49ff4c9caab542fb8e1dc64f9ec50f3
Haris Smajlović <smajlovicharis@hotmail.com>
2023-01-06 16:59:31 -0800

    Gaussian and uniform read_lvl vectorized
    Former-commit-id: 5d540b6afed72fb766cc0dd17046794699f128ca

--- ir/sequre.cpp
+++ ir/sequre.cpp
@@ -6,7 +6,7 @@ namespace sequre {
 
 void Sequre::addIRPasses(codon::ir::transform::PassManager *pm, bool debug) {
   // pm->registerPass(std::make_unique<Profiler>());
-  pm->registerPass(std::make_unique<ArithmeticsOptimizations>(), "core-folding-pass-group:2");
+  pm->registerPass(std::make_unique<ArithmeticsOptimizations>(), debug ? "" : "core-folding-pass-group:2");
 }
 
 } // namespace sequre
