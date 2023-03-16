e68239e9f68b2e3f2f46accf4cdf004df02fe374
Haris Smajlović <smajlovicharis@hotmail.com>
2023-03-15 23:32:40 -0700

    Upgraded Codon
    Former-commit-id: 6a21dea3785b722735088952d028f03e8d2285c9

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,10 +19,11 @@ else
     cp $CP_OPTIONS $SEQURE_STDLIB/network/inet_socket.codon $SEQURE_STDLIB/network/socket.codon
 fi
 
-echo "Seq plugin searched at $SEQ_PATH ..."
+echo "Numpy plugin searched at $SEQURE_NUMPY_PATH ..."
+echo "Seq plugin searched at $SEQURE_SEQ_PATH ..."
 echo "Sequre plugin searched at $SEQURE_PATH ..."
 echo "Compiling $2 in $1 mode ..."
-OMP_PROC_BIND=close codon run -plugin $SEQURE_PATH -plugin $SEQ_PATH -plugin $NUMPY_PATH $1 scripts/invoke.codon run-$2 ${*:3}
+OMP_PROC_BIND=close codon run -plugin $SEQURE_PATH -plugin $SEQURE_SEQ_PATH -plugin $SEQURE_NUMPY_PATH $1 scripts/invoke.codon run-$2 ${*:3}
 
 echo "Cleaning up sockets ..."
 find  . -name 'sock.*' -exec rm {} \;
