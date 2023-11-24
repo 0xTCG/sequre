524ed6e77c93f33f9ca60e024966bb80bb0d86b4
Haris Smajlović <smajlovicharis@hotmail.com>
2023-11-23 22:39:34 -0800

    IR order updated

--- ir/sequre.cpp
+++ ir/sequre.cpp
@@ -7,7 +7,7 @@
 namespace sequre {
 
 void Sequre::addIRPasses( codon::ir::transform::PassManager *pm, bool debug ) {
-  pm->registerPass(std::make_unique<ExpressivenessTransformations>(), debug ? "" : "core-pythonic-list-addition-opt");
+  pm->registerPass(std::make_unique<ExpressivenessTransformations>(), debug ? "" : "core-folding-pass-group:2");
   if ( !debug ) {
     pm->registerPass(std::make_unique<MPCOptimizations>(), "sequre-expressiveness-transformation");
     pm->registerPass(std::make_unique<MHEOptimizations>(), "sequre-mpc-opt");
