c3eee7049793b696b5fb34ace77d4dff8a56c90e
Haris Smajlović <smajlovicharis@hotmail.com>
2023-11-24 12:13:24 -0800

    Polynomial approximation constants added

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Cleaning up sockets ..."
-find  . -name 'sock.*' -exec rm {} \;
+find . -name 'sock.*' -exec rm {} \;
 
 if [[ -z "${SEQURE_CODON_PATH}" ]]; then
     echo "Error! SEQURE_CODON_PATH env variable not set" >&2
@@ -18,23 +18,23 @@ if [[ -z "${SEQURE_PATH}" ]]; then
     return
 fi
 
-if [ ! -d "${SEQURE_CODON_PATH}/install" ]
+if [ ! -d "${SEQURE_CODON_PATH}" ]
 then
     echo "Codon not installed at ${SEQURE_CODON_PATH}" >&2
     return
 fi
 
-if [ ! -d "${SEQURE_SEQ_PATH}/install" ]
+if [ ! -d "${SEQURE_SEQ_PATH}" ]
 then
     echo "Seq-lang not installed at ${SEQURE_SEQ_PATH}" >&2
     return
 fi
 
-echo "Codon found at $SEQURE_CODON_PATH"
-echo "Seq-lang plugin found at $SEQURE_SEQ_PATH"
-echo "Sequre plugin found at $SEQURE_PATH"
+echo "Codon path: $SEQURE_CODON_PATH"
+echo "Seq-lang plugin path: $SEQURE_SEQ_PATH"
+echo "Sequre plugin path: $SEQURE_PATH"
 echo "Compiling $2 in $1 mode ..."
-GC_INITIAL_HEAP_SIZE=8179869184 $SEQURE_CODON_PATH/build/codon run -plugin $SEQURE_PATH -plugin $SEQURE_SEQ_PATH $1 scripts/invoke.codon run-$2 ${*:3}
+GC_INITIAL_HEAP_SIZE=8179869184 $SEQURE_CODON_PATH/bin/codon run -plugin $SEQURE_PATH -plugin $SEQURE_SEQ_PATH $1 scripts/invoke.codon run-$2 ${*:3}
 
 echo "Cleaning up sockets ..."
-find  . -name 'sock.*' -exec rm {} \;
+find . -name 'sock.*' -exec rm {} \;
