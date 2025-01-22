f66e277add20a9d7b51409fb54552fb31a9a1498
hsmajlovic <hsmajlovic@uvic.ca>
2025-01-21 22:40:24 -0800

    Lattigo artifact preliminary done

--- applications/others/lattigo_king/batch_run.sh
+++ applications/others/lattigo_king/batch_run.sh
@@ -7,9 +7,49 @@ source ~/.bashrc
 
 START=0
 END=2
-TESTNAME=TestMatMultProtocol
 LOG_PREFIX=stdout/runtime
 TRY=1
+
+# toml set --toml-path='config/matmult/configLocal.Party1.toml' --to-int 'number_of_rows' 8
+# toml set --toml-path='config/matmult/configLocal.Party2.toml' --to-int 'number_of_rows' 8
+# toml set --toml-path='config/king/configLocal.Party1.toml' --to-int 'number_of_columns' 8192
+# toml set --toml-path='config/king/configLocal.Party2.toml' --to-int 'number_of_columns' 8192
+
+toml set --toml-path='config/king/configGlobal.toml' --to-bool 'minimum' false
+toml set --toml-path='config/king/configGlobal.toml' --to-bool 'sqrt' true
+
+TESTNAME=TestKingProtocol
+
+for (( i = $START; i <= $END; i++ )) 
+do
+  echo "Running PID=$i"
+  CMD="PID=$i go test -run ${TESTNAME} -timeout 48h | tee /dev/tty > ${LOG_PREFIX}_party${i}_try${TRY}.txt"
+  if [ $i = $END ]; then
+    eval $CMD
+  else
+    eval $CMD &
+  fi
+done
+
+toml set --toml-path='config/king/configGlobal.toml' --to-bool 'minimum' true
+toml set --toml-path='config/king/configGlobal.toml' --to-bool 'sqrt' false
+
+TESTNAME=TestMatMultProtocol
+
+for (( i = $START; i <= $END; i++ )) 
+do
+  echo "Running PID=$i"
+  CMD="PID=$i go test -run ${TESTNAME} -timeout 48h | tee /dev/tty > ${LOG_PREFIX}_party${i}_try${TRY}.txt"
+  if [ $i = $END ]; then
+    eval $CMD
+  else
+    eval $CMD &
+  fi
+done
+
+toml set --toml-path='config/matmult/configGlobal.toml' --to-bool 'ATA' true
+toml set --toml-path='config/matmult/configGlobal.toml' --to-bool 'ATA' true
+
 for (( i = $START; i <= $END; i++ )) 
 do
   echo "Running PID=$i"
