1114044eca970fb3bea0c866de4a12584a4ba0cd
Haris Smajlović <smajlovicharis@hotmail.com>
2025-02-13 21:18:42 -0800

    Lattigo artifact update

--- applications/others/lattigo_king/batch_run_basic.sh
+++ applications/others/lattigo_king/batch_run_basic.sh
@@ -11,6 +11,11 @@ END=2
 LOG_PREFIX=stdout/runtime
 TRY=1
 
+toml set --toml-path='config/king/configLocal.Party1.toml' 'simple_data_path' '/data/king_sample.txt'
+toml set --toml-path='config/king/configLocal.Party2.toml' 'simple_data_path' '/data/king_sample.txt'
+toml set --toml-path='config/matmult/configLocal.Party1.toml' 'simple_data_path' '/data/king_sample.txt'
+toml set --toml-path='config/matmult/configLocal.Party2.toml' 'simple_data_path' '/data/king_sample.txt'
+
 TESTNAME=TestMatMultProtocol
 
 for (( i = $START; i <= $END; i++ )) 
@@ -25,7 +30,6 @@ do
 done
 
 toml set --toml-path='config/matmult/configGlobal.toml' --to-bool 'ATA' true
-toml set --toml-path='config/matmult/configGlobal.toml' --to-bool 'ATA' true
 
 for (( i = $START; i <= $END; i++ )) 
 do
@@ -37,27 +41,8 @@ do
     eval $CMD &
   fi
 done
-
-toml set --toml-path='config/king/configLocal.Party1.toml' 'simple_data_path' '/data/king_sample.txt'
-toml set --toml-path='config/king/configLocal.Party2.toml' 'simple_data_path' '/data/king_sample.txt'
-toml set --toml-path='config/matmult/configLocal.Party1.toml' 'simple_data_path' '/data/king_sample.txt'
-toml set --toml-path='config/matmult/configLocal.Party2.toml' 'simple_data_path' '/data/king_sample.txt'
 
 TESTNAME=TestKingProtocol
-
-toml set --toml-path='config/king/configGlobal.toml' --to-bool 'minimum' true
-toml set --toml-path='config/king/configGlobal.toml' --to-bool 'sqrt' false
-
-for (( i = $START; i <= $END; i++ )) 
-do
-  echo "Running PID=$i"
-  CMD="PID=$i go test -run ${TESTNAME} -timeout 48h | tee /dev/tty > ${LOG_PREFIX}_party${i}_try${TRY}.txt"
-  if [ $i = $END ]; then
-    eval $CMD
-  else
-    eval $CMD &
-  fi
-done
 
 toml set --toml-path='config/king/configLocal.Party1.toml' --to-int 'number_of_columns' 8192
 toml set --toml-path='config/king/configLocal.Party2.toml' --to-int 'number_of_columns' 8192
