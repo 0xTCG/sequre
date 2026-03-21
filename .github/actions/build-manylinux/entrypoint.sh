ea84d107c4d12bff5bac91bc5636c784ec1c2f49
Haris Smajlović <hsmajlov@gmail.com>
2026-03-21 19:40:34 -0400

    Vibe hotfix: build script

--- .github/actions/build-manylinux/entrypoint.sh
+++ .github/actions/build-manylinux/entrypoint.sh
@@ -16,7 +16,8 @@ esac
 mkdir $HOME/.codon
 cd $HOME/.codon
 curl -L https://github.com/exaloop/codon/releases/download/v0.17.0/codon-$(uname -s | awk '{print tolower($0)}')-$(uname -m).tar.gz | tar zxvf - --strip-components=1
-cd /
+mkdir -p $OPT
+cd $OPT
 curl -L https://github.com/exaloop/llvm-project/releases/download/codon-15.0.1/llvm-$(uname -s | awk '{print tolower($0)}')-$(uname -m).tar.gz | tar zxvf -
 cd $HOME
 curl -L https://github.com/exaloop/seq/releases/download/v0.11.3/seq-$(uname -s | awk '{print tolower($0)}')-$(uname -m).tar.gz | tar zxvf - -C .codon/lib/codon/plugins
